f7065f2c0f3c14262f7570e57e8bfea97f074e7b
at309407 <anitiw01@iba.dk>
2024-04-27 22:15:36 +0200

    fetching data with rest-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react'
+import Header from "./components/Header";
+import ProductList from './components/ProductList';
+
+
 
 function App() {
+
+const [products,setProducts]=useState([]);
+
+useEffect(()=>{
+  async function getProducts(){
+    const res =await fetch ("https://kea-alt-del.dk/t7/api/products");
+    const data =await res.json();
+    setProducts(data);
+  }
+getProducts();
+},[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <div className='product grid-cols-1 md:flex gap-5 '>
+     <ProductList className="shrink " products={products} />
+     </div>
     </div>
   );
 }
